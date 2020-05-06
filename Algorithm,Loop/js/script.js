c40816e12f56520b6c98c84f56ee08a7ed1187c8
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-05-06 13:51:37 +0400

    Add files via upload

---
+++ Algorithm,Loop/js/script.js
@@ -0,0 +1,80 @@
+// var num=10000000001;
+
+// if(num>0)
+// {
+//     if(num%2==0)
+//     {
+//         console.log("Eded cutdur");
+//     }
+//     else
+//     {
+//         console.log("Eded tekdir");
+//     }
+// }
+// else
+// {
+//     console.log("Zehmet olmasa 0-dan boyuk eded daxil edin");
+// }
+
+// var iqiymet=-30;
+// var sqiymet=50;
+
+// if(iqiymet>=0 && iqiymet<=100 && sqiymet<=100 && sqiymet>=0)
+// {
+//     var result=sqiymet*0.5+iqiymet*0.5;
+//     if(result>50){
+//         console.log("Tebrikler");
+//     }
+//     else{
+//         console.log("Kesildiniz !!!");
+//     }
+// }
+// else
+// {
+//     console.log("Zehmet olmasa duzgun deyerler daxil edin !!!");
+// }
+
+// var i=0;
+
+// while(i<10)
+// {
+//     i++;
+//     if(i%4==2)
+//         console.log(i);
+    
+// }
+
+// var i=10;
+
+// while(i<10){
+//     console.log(i);
+// }
+
+// do{
+//     console.log(i);
+// }while(i<10)
+
+// for(var i=10; i>=0; i--){
+//     console.log(i);
+// }
+
+// for(var i=0; i<=10; i++){
+//     console.log(i);
+// }
+
+
+var min=-5;
+
+var max=-6;
+
+if(min<max){
+    for(var i=min;i<=max;i++){
+        console.log(i);
+    }
+}
+else{
+    console.log("Zehmet olmasa duzgun daxil edin")
+}
+
+
+
